20a8f656b4006d7186d10a289f4c0d11383035c8
Diego Izaguirre <tenochca.di@outlook.com>
2024-06-22 16:56:47 -0500

    app is in working state -- takes user input and provides classification

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer #type: ignore
+from keras.preprocessing.sequence import pad_sequences
+import pickle
+
+#loading in the model
+model = load_model('model.keras')
+
+#loading in the tokenizer
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+def prep_data(subject, body):
+    'prep subject and body txt data'
+    data = [subject + ' ' + body]
+    seq = tokenizer.texts_to_sequences(data)
+    X = pad_sequences(seq, maxlen=100)
+    return X
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        subject = request.form.get('sline')
+        body = request.form.get('body')
+
+        data = prep_data(subject, body)
+        pred = model.predict(data)
+        label = [1 if prob > 0.5 else 0 for prob in pred]
+
+        if label.pop == 1:
+            return "SCAM"
+        else:
+            return 'OK'
+    else:
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
